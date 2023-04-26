43bdd5dee33418b4c20dbb42023187abdf10d508
Perryjay30 <pelumijsh@gmail.com>
2023-04-26 17:25:37 +0100

    Room Availability

--- src/main/java/com/codingchallenge/roomoccupancy/data/RoomAvailability.java
+++ src/main/java/com/codingchallenge/roomoccupancy/data/RoomAvailability.java
@@ -2,6 +2,8 @@ package com.codingchallenge.roomoccupancy.data;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class RoomAvailability {
 //    private List<Double> potentialGuests;
